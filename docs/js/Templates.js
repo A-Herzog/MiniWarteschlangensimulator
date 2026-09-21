0d2b087cb13f8555749344e84ca200f76aca08fb
Alexander Herzog <alexander.herzog@gmx.de>
2026-09-21 07:47:50 +0200

    Updated signal source station colors

--- docs/js/Templates.js
+++ docs/js/Templates.js
@@ -436,7 +436,7 @@ templates.push({
   name: language.templates.signalSource,
   maxEdgesIn: 0,
   maxEdgesOut: 1,
-  addFunc: (id, nr, top, left, setup, isTemplate, elements)=>addBox("SignalSource",id,language.templates.signalSource+"<br>"+nr,language.templates.signalSourceHint,"#FB3","#FD7",top,left,isTemplate),
+  addFunc: (id, nr, top, left, setup, isTemplate, elements)=>addBox("SignalSource",id,language.templates.signalSource+"<br>"+nr,language.templates.signalSourceHint,"#FB3","#1C1",top,left,isTemplate),
   setup: {signal: '', b: 1}
 });
 templates.push({
